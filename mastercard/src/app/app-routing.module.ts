f267d409a0a2f1e9222de3c27b8983c657e22226
Pablo Say <19001434@galileo.edu>
2021-09-09 20:53:20 -0600

    Menu arreglado

--- mastercard/src/app/app-routing.module.ts
+++ mastercard/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { IngresarComponent } from './ingresar/ingresar.component';
+import { RegistrarComponent } from './registrar/registrar.component';
+const routes: Routes = [
+  {path: 'ingresar', component: IngresarComponent},
+  {path: 'registrar', component: RegistrarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- mastercard/src/app/app.module.ts
+++ mastercard/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { RegistrarComponent } from './registrar/registrar.component';
+import { IngresarComponent } from './ingresar/ingresar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RegistrarComponent,
+    IngresarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
