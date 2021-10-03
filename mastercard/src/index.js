0ca027712910d3158fa070178bb65dbe9af7fdff
Pablo Say <19001434@galileo.edu>
2021-10-03 11:21:12 -0600

    Todo unido

--- mastercard/src/index.js
+++ mastercard/src/index.js
@@ -1,5 +1,6 @@
 const app = require('./config/server');
 require('./app/routes/ingresar')(app);
 require('./app/routes/tarjetas')(app);
+require('./app/rutas/auto')(app);
 app.listen(app.get('port'),
     () => console.log(`server corriendo en puerto ${app.get('port')}`));
